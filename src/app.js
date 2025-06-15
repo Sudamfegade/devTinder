95c2af48c2802e290caed43133e198c21e7fd745
Sudam Fegade <sudamfegadepbn@gmail.com>
2025-06-15 08:06:20 +0530

    exploaring get post delete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 
 const app = express();
-
-app.use((req, res) => {
-  res.send("hello");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Sudam" });
 });
+
+app.post("/user", (req, res) => {
+  res.send("User created.");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted.");
+});
+
+// app.use("/Hello", (req, res) => {
+//   res.send("new helo");
+// });
+// app.use("/", (req, res) => {
+//   res.send("hello");
+// });
 
 app.listen(3000, () => console.log("server is listening on port 3000..."));
