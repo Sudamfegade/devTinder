d5f9338a9c9c41b2328b37b50fcfeded0c92a110
Sudam Fegade <sudamfegadepbn@gmail.com>
2025-07-15 09:41:03 +0530

    keysec

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://sudamfegade:Z0m2Lb56QsyxlgNv@namastenode.4ssb6vr.mongodb.net/devTinder"
+    "mongodb+srv://sudamfegade:@namastenode.4ssb6vr.mongodb.net/devTinder"
   );
 };
 
